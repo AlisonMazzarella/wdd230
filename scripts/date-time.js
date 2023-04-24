dba3fbdef0e778baf161846a270099a33099b98a
AlisonMazzarella <maz12005@byui.edu>
2023-04-24 18:12:27 +0200

    removed date formatting per teacher instructions

--- scripts/date-time.js
+++ scripts/date-time.js
@@ -2,15 +2,11 @@
 const footer = document.querySelector('footer');
 
 //get date and time web file was last modified
-const lastModified = new Date(document.lastModified);
-
-//formate date and time as a string 
-const formattedDate = lastModified.toLocaleDateString('en-US');
-const formattedTime = lastModified.toLocaleTimeString('en-US');
+const lastModified = document.lastModified;
 
 //create paragraph to display date and time and current year 
 const dateElement = document.createElement('p');
-dateElement.textContent = `Last updated: ${formattedDate} ${formattedTime}`;
+dateElement.textContent = document.lastModified;
 
 //add paragraph to footer
 footer.appendChild(dateElement);
