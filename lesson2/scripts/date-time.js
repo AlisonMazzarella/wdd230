b72b6fc4138063db6e0daba709a0fa188f1dc1a3
AlisonMazzarella <maz12005@byui.edu>
2023-04-24 20:58:43 +0200

    fixed l2 javascript too

--- lesson2/scripts/date-time.js
+++ lesson2/scripts/date-time.js
@@ -4,9 +4,12 @@ const footer = document.querySelector('footer');
 //get date and time web file was last modified
 const lastModified = document.lastModified;
 
+// get current year
+const currentYear = new Date().getFullYear();
+
 //create paragraph to display date and time and current year 
 const dateElement = document.createElement('p');
-dateElement.textContent = document.lastModified;
+dateElement.textContent = `${currentYear}  -  Alison Mazzarella-Woelzl  -  Austria  - Last updated: ${lastModified}`;
 
 //add paragraph to footer
 footer.appendChild(dateElement);
