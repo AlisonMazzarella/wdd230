231c2f6258d2ef06804938585542d103780ca598
AlisonMazzarella <maz12005@byui.edu>
2023-05-08 07:04:50 +0200

    fixed bold in js

--- chamber/scripts/modification.js
+++ chamber/scripts/modification.js
@@ -5,6 +5,6 @@ const lastModified = document.lastModified;
 const currentYear = new Date().getFullYear();
 
 const dateElement = document.createElement('p');
-dateElement.textContent = `\u00A9 ${currentYear}  Peachtree City Chamber | Alison Mazzarella-Woelzl | WDD 230 Project | Last Modification: ${lastModified}`;
+dateElement.innerHTML = `\u00A9 ${currentYear}  Peachtree City Chamber | <b>Alison Mazzarella-Woelzl</b> | WDD 230 Project | Last Modification: ${lastModified}`;
 
 footer.appendChild(dateElement);
