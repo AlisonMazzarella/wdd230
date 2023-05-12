0d6f3795df302c74653e1826125c505f0948a01f
AlisonMazzarella <maz12005@byui.edu>
2023-05-12 15:37:36 +0200

    small view done

--- chamber/scripts/modification.js
+++ chamber/scripts/modification.js
@@ -1,6 +1,7 @@
 const modificationOutput = document.getElementById('modificationOutput');
 
-const lastModified = document.lastModified;
+const lastModified = new Date(document.lastModified);
 const currentYear = new Date().getFullYear();
+const lastModifiedTime = lastModified.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
 
-modificationOutput.innerHTML = `\u00A9 ${currentYear}  Peachtree City Chamber | <b>Alison Mazzarella-Woelzl</b> | WDD 230 Project | Last Modification: ${lastModified}`;
+modificationOutput.innerHTML = `\u00A9 ${currentYear}  Peachtree City Chamber | Alison Mazzarella-Woelzl | WDD 230 Project | Last Modification: ${lastModified.toLocaleDateString()} ${lastModifiedTime}`;
