6fe84e8d4bb90e17c238114a95bb91383c1654cc
AlisonMazzarella <maz12005@byui.edu>
2023-05-17 19:31:11 +0200

    updated based on feedback

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var currentDateContainer = document.getElementById('currentDate');
-    var currentDate = new Date();
+    const currentDateContainer = document.getElementById('currentDate');
+    const currentDate = new Date();
     
-    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = currentDate.toLocaleDateString(undefined, options);
     
-    var formattedDate = daysOfWeek[currentDate.getDay()] + ', ' + currentDate.getDate() + ' ' + months[currentDate.getMonth()] + ' ' + currentDate.getFullYear();
-    
-    if (currentDate.getDay() === 1) {
-        formattedDate += "\n🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    if (currentDate.getDay() === 1 || currentDate.getDay() === 2) {
+        const localizedDay = currentDate.toLocaleDateString(undefined, { weekday: 'long' });
+        formattedDate += `\n🤝🏼 Come join us for the chamber meet and greet ${localizedDay} at 7:00 p.m.`;
     }
     
     currentDateContainer.innerText = formattedDate;
