4f9a6e80be0f8b2c505dde3822beea268ea6f4d8
AlisonMazzarella <maz12005@byui.edu>
2023-05-30 14:26:14 -0400

    changed const to var

--- lesson7/scripts/lazyload.js
+++ lesson7/scripts/lazyload.js
@@ -1,12 +1,12 @@
-const images = document.querySelectorAll("img[data-src]");
+var images = document.querySelectorAll("img[data-src]");
 
 function preloadImage(img) {
-  const src = img.getAttribute("data-src");
+  var src = img.getAttribute("data-src");
   if (!src) {
     return;
   }
 
-  const newImg = new Image();
+  var newImg = new Image();
   newImg.src = src;
   newImg.addEventListener("load", () => {
     img.src = src;
@@ -15,12 +15,12 @@ function preloadImage(img) {
   });
 }
 
-const imgOptions = {
+var imgOptions = {
   threshold: 0,
   rootMargin: "0px 0px -200px 0px"
 };
 
-const imgObserver = new IntersectionObserver((items, imgObserver) => {
+var imgObserver = new IntersectionObserver((items, imgObserver) => {
   items.forEach((item) => {
     if (!item.isIntersecting) {
       return;
