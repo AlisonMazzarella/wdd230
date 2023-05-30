edf09f15c6fb101754597e9e3eb07349c05aaaae
AlisonMazzarella <maz12005@byui.edu>
2023-05-30 14:22:24 -0400

    fixed lazyload

---
+++ lesson7/scripts/date-time.js
@@ -0,0 +1,15 @@
+//footer element
+const footer = document.querySelector('footer');
+
+//get date and time web file was last modified
+const lastModified = document.lastModified;
+
+// get current year
+const currentYear = new Date().getFullYear();
+
+//create paragraph to display date and time and current year 
+const dateElement = document.createElement('p');
+dateElement.textContent = `${currentYear}  -  Alison Mazzarella-Woelzl  -  Austria  - Last updated: ${lastModified}`;;
+
+//add paragraph to footer
+footer.appendChild(dateElement);
---
+++ lesson7/scripts/lazyload.js
@@ -0,0 +1,36 @@
+const images = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+
+  const newImg = new Image();
+  newImg.src = src;
+  newImg.addEventListener("load", () => {
+    img.src = src;
+    img.removeAttribute("data-src");
+    img.setAttribute("data-loaded", "true");
+  });
+}
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px"
+};
+
+const imgObserver = new IntersectionObserver((items, imgObserver) => {
+  items.forEach((item) => {
+    if (!item.isIntersecting) {
+      return;
+    } else {
+      preloadImage(item.target);
+      imgObserver.unobserve(item.target);
+    }
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
