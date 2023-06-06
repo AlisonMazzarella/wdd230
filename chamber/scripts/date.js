5ef39c9e9db72a773e9512de69432290ad015264
AlisonMazzarella <maz12005@byui.edu>
2023-06-06 09:20:12 -0400

    fixed date.js

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const currentDateContainer = document.getElementById('currentDate');
-    const currentDate = new Date();
+    var currentDateContainer = document.getElementById('currentDate');
+    var currentDate = new Date();
     
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = currentDate.toLocaleDateString(undefined, options);
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let formattedDate = currentDate.toLocaleDateString(undefined, options);
     
-    if (currentDate.getDay() === 1 || currentDate.getDay() === 2) {
+    if (window.location.pathname === '/chamber/index.html' && (currentDate.getDay() === 1 || currentDate.getDay() === 2)) {
         const localizedDay = currentDate.toLocaleDateString(undefined, { weekday: 'long' });
         formattedDate += `\n🤝🏼 Come join us for the chamber meet and greet ${localizedDay} at 7:00 p.m.`;
     }
