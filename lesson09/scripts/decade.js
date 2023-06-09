d3095983b259b6945a635892f9ca5fcce3cadcd1
AlisonMazzarella <maz12005@byui.edu>
2023-06-09 10:53:35 -0400

    removed decade

--- lesson09/scripts/decade.js
+++
@@ -1,8 +0,0 @@
-var filterBtn = document.getElementById('filterBtn');
-
-filterBtn.addEventListener('click', filterProphetsByDecade);
-
-function filterProphetsByDecade() {
-  var decadeProphets = prophets.filter(prophet => prophet.length >= 10);
-  displayProphets(decadeProphets);
-}
