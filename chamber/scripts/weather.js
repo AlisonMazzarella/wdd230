9a01ace8c07d5209a611307aed457e0b998855b5
AlisonMazzarella <maz12005@byui.edu>
2023-06-20 12:13:00 -0400

    weather working now

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,32 +1,44 @@
-const apiKey = 'd1ec9825425f7e5c775640a0608faebf';
-const location = 'Peachtree City, GA';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`;
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=d1ec9825425f7e5c775640a0608faebf';
 
 async function fetchWeatherData() {
-    try {
-      const response = await fetch(apiUrl);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
       const data = await response.json();
-      updateWeatherContent(data);
-    } catch (error) {
-      console.log('Error fetching weather data:', error);
+      displayResults(data);
+    } else {
+      throw new Error('Error fetching weather data:', response.status);
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function updateWeatherContent(data) {
-    document.getElementById('weatherIcon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    document.getElementById('weatherCondition').textContent = data.weather[0].main;
-    document.getElementById('windSpeed').textContent = data.wind.speed + ' mph';
-    document.getElementById('temperature').textContent = data.main.temp;
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>°F`;
 
-const windChill = calculateWindChill(data.main.temp, data.wind.speed);
-document.getElementById('windChill').textContent = windChill !== null ? windChill + '°F' : 'N/A';
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+  const windSpeed = weatherData.wind.speed;
+  const windChill = calculateWindChill(weatherData.main.temp, windSpeed);
+  document.getElementById('windSpeed').textContent = `${windSpeed} mph`;
+  document.getElementById('windChill').textContent = windChill !== null ? `${windChill}°F` : 'N/A';
 }
 
 function calculateWindChill(temperature, windSpeed) {
-    if (temperature <= 50 && windSpeed >= 3) {
-      return Math.round(35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16));
-    }
-    return null; 
+  if (temperature <= 50 && windSpeed >= 3) {
+    return Math.round(35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16));
+  }
+  return null;
 }
 
-window.addEventListener('load', fetchWeatherData);
+fetchWeatherData();
