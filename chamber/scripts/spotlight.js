95a9668ecaffb8fd35463783117a5dfc9972024b
AlisonMazzarella <maz12005@byui.edu>
2023-06-28 10:31:44 -0400

    fixed spotlights

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -23,19 +23,19 @@ function displaySpotlightMembers(members) {
     document.getElementById("spotlight1Name").textContent = spotlight1.name;
     document.getElementById("spotlight1Icon").src = spotlight1.imageurl;
     document.getElementById("spotlight1Description").textContent = spotlight1.services;
-    document.getElementById("spotlight1Email").textContent = spotlight1.number;
+    document.getElementById("spotlight1Number").textContent = spotlight1.number;
   
     const spotlight2 = getRandomSpotlightMember(members);
     document.getElementById("spotlight2Name").textContent = spotlight2.name;
     document.getElementById("spotlight2Icon").src = spotlight2.imageurl;
     document.getElementById("spotlight2Description").textContent = spotlight2.services;
-    document.getElementById("spotlight2Email").textContent = spotlight2.number;
+    document.getElementById("spotlight2Number").textContent = spotlight2.number;
   
     const spotlight3 = getRandomSpotlightMember(members);
     document.getElementById("spotlight3Name").textContent = spotlight3.name;
     document.getElementById("spotlight3Icon").src = spotlight3.imageurl;
     document.getElementById("spotlight3Description").textContent = spotlight3.services;
-    document.getElementById("spotlight3Email").textContent = spotlight3.number;
+    document.getElementById("spotlight3Number").textContent = spotlight3.number;
 }
   
 fetchDirectoryData();
